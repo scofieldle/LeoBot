e30ccaf8a0c921a1ec3f2873f459cd626b40fa6c
scofieldle <798222964@qq.com>
2021-01-05 11:11:48 +0800

    减少奇怪log

--- hoshino/modules/priconne/chara.py
+++ hoshino/modules/priconne/chara.py
@@ -46,8 +46,6 @@ class Roster:
                 n = util.normalize_str(n)
                 if n not in self._roster:
                     self._roster[n] = idx
-                else:
-                    logger.warning(f'priconne.chara.Roster: 出现重名{n}于id{idx}与id{self._roster[n]}')
         self._all_name_list = self._roster.keys()
 
 
